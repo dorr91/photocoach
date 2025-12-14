3ef6e5d17b3e07079da039b5f8298ad5e7ae5c2b
David Orr <david@davidrorr.com>
2025-12-14 11:05:48 -0500

    Initial Commit

---
+++ PhotoCoach/PhotoCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCoachApp.swift
+//  PhotoCoach
+//
+//  Created by David Orr on 12/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
